ddd9be79fa368c0098b26512802f7dca59ccc6e8
NONSTOPGAMING <kyle.swot.2018@gmail.com>
2014-03-20 17:31:37 +0000

    Added until block to let user guess secret number.

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,3 +1,12 @@
 puts"Welcome to the game"
 secret_number = rand(6)
-puts secret_number
+
+puts "guess the number between 0 and 5"
+guess = gets.chomp.to_i
+
+until guess == secret_number 
+  puts "Wrong! Try again."
+guess = gets.chomp.to_i
+end
+
+puts "Congrats! You won!"
